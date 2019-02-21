b8ff024974fbfa2a8eaddcf3b904e4370ca2701d
LighthawkKnight <lighthawkknight@gmail.com>
2019-02-21 01:41:14 -0800

    static

--- server.js
+++ server.js
@@ -22,8 +22,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 app.use(passport.initialize());
 // Serve up static assets
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+}
+
+else {
+  app.use(express.static(path.join(__dirname, '/client/public')));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
 }
 app.use(session({
   secret: 'countdown-project',
